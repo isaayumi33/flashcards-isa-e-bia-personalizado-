bc0729fa121744ad8bf1bd413ecfea6830d7536b
isaayumi33 <133604329+isaayumi33@users.noreply.github.com>
2024-11-12 20:03:58 -0300

    Add files via upload

---
+++ Flashcard-isa-e-bia_-main 4/Flashcard-isa-e-bia_-main/flashcard isa e bia/perguntas.js
@@ -0,0 +1,72 @@
+
+
+criaCartao(
+    'Ditados populares',
+    'Filho de peixe...',
+    'peixinho é!'
+)
+
+criaCartao (
+    'Ditados populares',
+    'Cão que late...',
+    'não morde!'
+)
+
+criaCartao(
+    'Ditados populares',
+    'Água mole em pedra dura...',
+    'tanto bate até que fura!'
+)
+
+criaCartao(
+    'Ditados populares',
+    'Antes tarde...',
+    'do que nunca!'
+)
+
+criaCartao(
+    'Ditados populares',
+    'Quem ri por último...',
+    'ri melhor!'
+)
+
+criaCartao(
+    'Ditados populares',
+    'A pressa é inimiga...',
+    'da perfeição!'
+)
+
+criaCartao(
+    'Ditados populares',
+    'Quem tem boca...',
+    'vai a Roma!'
+)
+ criaCartao(
+    'Ditados populares',
+    'Deus ajuda...',
+    'quem cedo madruga!'
+ )
+
+ criaCartao(
+    'Ditados populares',
+    'Quem é vivo...',
+    'sempre aparece!'
+ )
+ 
+ criaCartao(
+    'Ditados populares',
+    'Pimenta no cu dos outros...',
+    'é refresco!'
+ )
+
+ criaCartao(
+    'Ditados populares',
+    'O barato...',
+    'sai caro!'
+ )
+
+ criaCartao(
+    'Ditados populares',
+    'Quando um não quer...',
+    'dois não brigam'
+ )
